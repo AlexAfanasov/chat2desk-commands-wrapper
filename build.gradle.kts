a5fa5e5bfc64a16853ea1047715d31ab44132ded
AlexAfanasov <afan.lexx@gmail.com>
2026-04-15 13:25:51 +0300

    fix(build): Fix publishing wrapper via JitPack

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,13 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.25" apply false
 }
 
+val jitpackVersion = System.getenv("VERSION")
+
 subprojects {
     group =
         (rootProject.findProperty("POM_GROUP") as String?)
             ?: "com.github.AlexAfanasov.chat2desk-commands-wrapper"
-    version = (rootProject.findProperty("VERSION_NAME") as String?) ?: "0.1.0-SNAPSHOT"
+    version = jitpackVersion ?: (rootProject.findProperty("VERSION_NAME") as String?) ?: "0.1.0-SNAPSHOT"
 }
 
 tasks.register<Delete>("clean") {
